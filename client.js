36de482e557db3741207aaa88156df4c21ec7083
Austin Kammerer <austin4191@gmail.com>
2021-10-04 16:14:29 -0500

    added pics to DOM

--- client.js
+++ client.js
@@ -1,1 +1,17 @@
-console.log('Here are all the available people:', people);
+console.log("Here are all the available people:", people);
+
+$(readyNow);
+
+function readyNow() {
+  console.log("JQ");
+  for (let person of people) {
+    let face = $(`
+  <div class="face">
+    <img src="https://github.com/${person.githubUsername}.png?size=250" alt="Profile image of ${person.name}">
+
+  </div>
+  `);
+    $("body").append(face);
+    $("#nameToGuess").text(person.name);
+  }
+}
