f20f2f83087d75fb9cbe26b8da8c169859977f8a
Austin Kammerer <austin4191@gmail.com>
2021-10-04 16:35:28 -0500

    adds event listener and checkIfCorrect()

--- client.js
+++ client.js
@@ -1,5 +1,7 @@
 console.log("Here are all the available people:", people);
 
+let nameToGuess = nameRandomizer();
+
 $(readyNow);
 
 function readyNow() {
@@ -12,9 +14,28 @@ function readyNow() {
   </div>
   `);
     $("body").append(face);
+    face.data(person);
   }
 
-  $("#nameToGuess").text(people[randomNumber(0, people.length - 1)].name);
+  $("#nameToGuess").text(nameToGuess);
+
+  // event listener
+  $(`.face`).on(`click`, checkIfCorrect);
+}
+
+function checkIfCorrect() {
+  console.log(`in check`);
+  let name = $(this).data().name;
+  console.log(name);
+  if (name === nameToGuess) {
+    console.log(`you got it`);
+  } else {
+    console.log(`you were wrong`);
+  }
+}
+
+function nameRandomizer() {
+  return people[randomNumber(0, people.length - 1)].name;
 }
 
 function randomNumber(min, max) {
